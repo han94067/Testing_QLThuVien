29df6b34743c68645145711c369bc870fd39ff4a
han94067 <han94067@gmail.com>
2017-09-25 00:28:44 +0700

    Test_14

---
+++ Testing_QLThuVien/Controllers/DocGiaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Testing_QLThuVien.Models;
+
+namespace Testing_QLThuVien.Controllers
+{
+    public class DocGiaController : Controller
+    {
+
+        QLThuVien db = new QLThuVien();
+
+        public ActionResult DSDocGia()
+        {
+            return View(db.DocGias.ToList());
+        }
+    }
+}
--- Testing_QLThuVien/Controllers/QLSachController.cs
+++ Testing_QLThuVien/Controllers/QLSachController.cs
@@ -16,27 +16,27 @@ namespace Testing_QLThuVien.Controllers
         {
             return View(db.Saches.ToList());
         }
-   
+
         [HttpGet]
         public ActionResult ThemSach()
         {
-            return View();
+            return PartialView();
         }
-        
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult ThemSach([Bind(Include = "MaSach, MaTacGia, MaNhaXuatBan, MaTheLoai, TenSach, HinhAnh, NamXuatBan, SoLuong, SoLuongTon, TriGia, TinhTrang")] Sach sach)
         {
             sach.MaSach = "";
-            sach.MaTacGia = "a2";
-            sach.MaNhaXuatBan = "a1";
-            sach.MaTheLoai = "a4";
+            sach.MaTacGia = "1";
+            sach.MaNhaXuatBan = "1";
+            sach.MaTheLoai = "1";
             sach.HinhAnh = "asd";
             sach.SoLuongTon = 10;
-            sach.TinhTrang = 1;        
+            sach.TinhTrang = 1;
             db.Saches.Add(sach);
             db.SaveChanges();
-            return View("Sach");
+            return RedirectToAction("Sach");
         }
     }
 }
